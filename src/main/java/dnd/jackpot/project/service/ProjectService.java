604992946d752932b5dd3319ba677497a6960cde
김종건 <48309937+kjg9704@users.noreply.github.com>
2021-02-09 04:15:09 +0900

    test

--- src/main/java/dnd/jackpot/project/service/ProjectService.java
+++ src/main/java/dnd/jackpot/project/service/ProjectService.java
@@ -4,7 +4,6 @@ import dnd.jackpot.project.dto.PagingDto;
 import dnd.jackpot.project.dto.ProjectDto;
 import dnd.jackpot.project.dto.ProjectModifyDto;
 import dnd.jackpot.project.dto.ProjectSaveDto;
-import dnd.jackpot.project.dto.ProjectSearchDto;
 
 public interface ProjectService {
 //	PagingDto<ProjectDto> findAll(ProjectSearchDto searchDto);
