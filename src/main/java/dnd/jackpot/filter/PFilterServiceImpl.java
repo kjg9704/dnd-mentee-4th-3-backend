86c4272777e5c00db408f737df21c4926a3a2a29
jola7373 <jola7373@naver.com>
2021-02-09 11:56:38 -0800

    [수정]임시파일

--- src/main/java/dnd/jackpot/filter/PFilterServiceImpl.java
+++ src/main/java/dnd/jackpot/filter/PFilterServiceImpl.java
@@ -4,22 +4,44 @@ import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
+import org.springframework.data.domain.Page;
+
 import dnd.jackpot.project.dto.ProjectDto;
+import dnd.jackpot.project.entity.Project;
+import dnd.jackpot.project.repository.ProjectRepository;
 
 public class PFilterServiceImpl implements PFilterService {
+	private final ProjectRepository repo;
+	
+	private List<Project> RprojectList;
+	private List<Project> SprojectList;
+	private List<Project> IprojectList;
 	
 	@Override 
 	public List<PFilterDto> getAll(PFilterSearchDto searchDto){
+		Page<Project> pageProjects;
 		if(Objects.nonNull(searchDto.getRegionFilter())) {
-			List<ProjectDto> projectDtoList = ProjectMapperService.toDto(pageProjects.getContent());
+//			 = ProjectMapperService.toDto(pageProjects.getContent());
+//			repo.findAll()->
+			RprojectList = repo.findByInterest(String a);
+		}else {
+			RprojectList = repo.findAll();
+//			필터를 적용하지 않는경우 다 가져온다.
+//			RprojectDtoList = get
 		}
 		if(Objects.nonNull(searchDto.getStackFilter())) {
-			List<ProjectDto> projectDtoList = ProjectMapperService.toDto(pageProjects.getContent());
+//			 = ProjectMapperService.toDto(pageProjects.getContent());
+		}else {
+			SprojectList = repo.findAll();
 		}
 		if(Objects.nonNull(searchDto.getInterestFilter())) {
-			List<ProjectDto> projectDtoList = ProjectMapperService.toDto(pageProjects.getContent());
+			
+		}else {
+			IprojectList = repo.findAll();
 		}
-		for()
+		if(!RprojectDtoList.isEmpty() && )
+		
+		
 		
 	}
 }
