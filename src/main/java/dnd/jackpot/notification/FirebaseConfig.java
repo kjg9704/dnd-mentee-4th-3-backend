e3547b189fd865a974a13d18109f551b531b090b
김종건 <48309937+kjg9704@users.noreply.github.com>
2021-02-22 00:42:11 +0900

    [수정] FCM키파일 절대경로지정

--- src/main/java/dnd/jackpot/notification/FirebaseConfig.java
+++ src/main/java/dnd/jackpot/notification/FirebaseConfig.java
@@ -4,9 +4,10 @@ import java.io.FileInputStream;
 
 import javax.annotation.PostConstruct;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
@@ -15,15 +16,20 @@ import com.google.firebase.FirebaseOptions;
 @Configuration
 public class FirebaseConfig {
 
-    @Value("classpath:firebase/jackpot-1611239774705-firebase-adminsdk-xlp80-fa2c872b91.json")
+//    @Value("classpath:firebase/jackpot-1611239774705-firebase-adminsdk-xlp80-fa2c872b91.json")
 //    @Value("classpath:firebase/jackpot-d5f0e-firebase-adminsdk-46a97-cc8373fa78.json")
-    private Resource resource;
+//    private Resource resource;
+    @Autowired
+    private ResourceLoader resourceLoader;
+    
+    final Resource fileResource = resourceLoader.getResource("classpath:firebase/jackpot-1611239774705-firebase-adminsdk-xlp80-fa2c872b91.json");
+
 
     @PostConstruct
     public void initFirebase() {
         try {
             // Service Account를 이용하여 Fireabse Admin SDK 초기화
-            FileInputStream serviceAccount = new FileInputStream(resource.getFile());
+            FileInputStream serviceAccount = new FileInputStream(fileResource.getFile());
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
   //                  .setDatabaseUrl("https://{사용자마다 다름}.firebaseio.com")
